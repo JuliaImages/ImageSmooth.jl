21e8b7468b834ea064e76c0512f91e57e2294e19
Documenter.jl <documenter@juliadocs.github.io>
2021-07-03 04:34:25 +0000

    build based on 9ffcda3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ImageSmooth","category":"page"},{"location":"#ImageSmooth","page":"Home","title":"ImageSmooth","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ImageSmooth.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ImageSmooth]","category":"page"}]
+}
