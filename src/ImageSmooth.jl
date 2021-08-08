4dd6c33e91406d4439fab00327eb03002323fda7
Johnny Chen <johnnychen94@hotmail.com>
2021-08-08 16:50:52 +0800

    remove empty include

--- src/ImageSmooth.jl
+++ src/ImageSmooth.jl
@@ -9,9 +9,6 @@ include("SmoothAPI/SmoothAPI.jl")
 import .SmoothAPI: AbstractImageSmoothAlgorithm,
                    smooth, smooth!
 
-include("utils.jl")
-include("compat.jl")
-
 # Smooth algorithms
 
 include("algorithms/l0_smooth.jl") # L0Smooth
